6f62b6590cc6a0fbf1e51b9a169813d597f934b9
techiee3041 <doreenoms6699@gmail.com>
2023-10-02 14:26:34 +0300

    error handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Building first API"""
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -18,6 +18,12 @@ app.register_blueprint(app_views)
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found_error():
+    """Handle 404 errors"""
+    return jsonify({'error': 'Not found'}), 404
 
 
 if __name__ == "__main__":
