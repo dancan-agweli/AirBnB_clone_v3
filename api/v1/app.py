706e3e06b59dd8345161f740a37d42bd98b31917
techiee3041 <doreenoms6699@gmail.com>
2023-10-02 22:27:55 +0300

    3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,29 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-'''Contains a Flask web application API.
-'''
-import os
-from flask import Flask, jsonify
-from flask_cors import CORS
-
-from models import storage
-from api.v1.views import app_views
-
-
-app = Flask(__name__)
-'''The Flask web application instance.'''
-app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-app.url_map.strict_slashes = False
-app.register_blueprint(app_views)
-CORS(app, resources={'/*': {'origins': app_host}})
-
-
-@app.teardown_appcontext
-def teardown_flask(exception):
-    '''The Flask app/request context end event listener.'''
-    # print(exception)
-=======
 """ Script that imports a Blueprint and runs Flask """
 from flask import Flask, make_response, jsonify
 from flask_cors import CORS
@@ -59,35 +34,10 @@ swagger = Swagger(app)
 @app.teardown_appcontext
 def teardown_session(exception):
     """ Closes storage session """
->>>>>>> 964d5a2bb34bda75468013d36b0b6706af437485
     storage.close()
 
 
 @app.errorhandler(404)
-<<<<<<< HEAD
-def error_404(error):
-    '''Handles the 404 HTTP error code.'''
-    return jsonify(error='Not found'), 404
-
-
-@app.errorhandler(400)
-def error_400(error):
-    '''Handles the 400 HTTP error code.'''
-    msg = 'Bad request'
-    if isinstance(error, Exception) and hasattr(error, 'description'):
-        msg = error.description
-    return jsonify(error=msg), 400
-
-
-if __name__ == '__main__':
-    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-    app.run(
-        host=app_host,
-        port=app_port,
-        threaded=True
-    )
-=======
 def not_found(error):
     """ Returns JSON response with 404 status """
     return make_response(jsonify({"error": "Not found"}), 404)
@@ -100,4 +50,3 @@ if __name__ == '__main__':
     host = '0.0.0.0' if not HBNB_API_HOST else HBNB_API_HOST
     port = 5000 if not HBNB_API_PORT else HBNB_API_PORT
     app.run(host=host, port=port, threaded=True)
->>>>>>> 964d5a2bb34bda75468013d36b0b6706af437485
